600e44ab298622f59e0be5dd05b19e4b3c8a93eb
CL3372 <chocolatesdomjose@gmail.com>
2026-02-02 13:47:53 +0000

    added search function using Apikey and ApiUrl

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
+}
+
+function searchCity(city) {
+  let apiKey = "aa9340df384978aa7t1a53a8fabcc2o1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Lisbon");

99c2a58d314efabcaa7795758747eba9340acad4
CL3372 <chocolatesdomjose@gmail.com>
2026-02-03 20:48:23 +0000

    added search for City and temperature with API weather link

--- src/index.js
+++ src/index.js
@@ -1,69 +1,24 @@
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windspeedElement = document.querySelector("#wind-speed");
-  let timeElement = document.querySelector("#time");
-  let date = new Date(response.data.time * 1000);
-
-let iconElement = document.querySelector("#icon");
-
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
-
-  
-  
-  cityElement.innerHTML = response.data.city;
-
- timeElement.innerHTML = formatDate(date);
-  descriptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
-icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />;
-
-
-function formatDate(date) {
-  if (!(date instanceof Date)) {
-    throw new Error('Invalid date object');
-  }
-
-  let minutes = date.getMinutes().toString().padStart(2, '0');
-  let hours = date.getHours().toString().padStart(2, '0');
-
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-
-  let day = days[date.getDay()];
-
-  return ${day}, ${hours}:${minutes};
 }
-
-if (minutes < 10) { 
-    minutes = 0${minutes};
-}
-
 
 function searchCity(city) {
   let apiKey = "aa9340df384978aa7t1a53a8fabcc2o1";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Lisbon");
